b73394172993eb83b194958421b9418d4a8afce7
dongmin <ehdals0227@gmail.com>
2020-07-16 18:10:21 +0900

    Fix: color값 오타 수정

--- src/util/color.js
+++ src/util/color.js
@@ -2,6 +2,6 @@ export const main = ["#fede22", "#06D0A9"];
 
 export const white = "#fff";
 
-export const black = "000";
+export const black = "#000";
 
 export const gray = ["#333", "#eee", "#fafafa", "#f9f9f9"];
