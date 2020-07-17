1004786e0bae81272abbb6d9d761419eef7df705
dongmin <ehdals0227@gmail.com>
2020-07-17 17:29:50 +0900

    Add: 레이아웃에 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from "react";
+import "./styles/font.css";
+import "./styles/reset.css";
+import "./styles/default.css";
+import Layout from "./components/Layout";
 import Nav from "./components/Nav";
+import SectionOne from "./components/SectionOne";
+import SectionTwo from "./components/SectionTwo";
+import SectionThree from "./components/SectionThree";
+import SectionFour from "./components/SectionFour";
+import SectionFive from "./components/SectionFive";
+import SectionSix from "./components/SectionSix";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <Nav />
+      <Layout>
+        <Nav />
+        <SectionOne />
+        <SectionTwo />
+        <SectionThree />
+        <SectionFour />
+        <SectionFive />
+        <SectionSix />
+
+        <Footer />
+      </Layout>
     </div>
   );
 }
