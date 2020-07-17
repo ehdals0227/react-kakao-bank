42932d4a7e186043f94bad3f100888d8ba20329d
dongmin <ehdals0227@gmail.com>
2020-07-17 17:27:24 +0900

    Add: 컬러값, 이미지 일부 추가

--- src/util/color.js
+++ src/util/color.js
@@ -4,4 +4,4 @@ export const white = "#fff";
 
 export const black = "#000";
 
-export const gray = ["#333", "#eee", "#fafafa", "#f9f9f9"];
+export const gray = ["#333", "#666", "#eee", "#f6f6f6", "#f9f9f9", "#fafafa"];
