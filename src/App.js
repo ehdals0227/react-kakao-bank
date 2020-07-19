a6f185127b848861914da44f00ffccf4a709cbf8
dongmin <ehdals0227@gmail.com>
2020-07-19 20:57:41 +0900

    Add: Layout에 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+
+// common styles
 import "./styles/font.css";
 import "./styles/reset.css";
 import "./styles/default.css";
+
+// slick-slider styles
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 import Layout from "./components/Layout";
 import Nav from "./components/Nav";
 import SectionOne from "./components/SectionOne";
@@ -10,9 +17,12 @@ import SectionThree from "./components/SectionThree";
 import SectionFour from "./components/SectionFour";
 import SectionFive from "./components/SectionFive";
 import SectionSix from "./components/SectionSix";
+import SectionSeven from "./components/SectionSeven";
+import SectionEight from "./components/SectionEight";
+import SectionNine from "./components/SectionNine";
 import Footer from "./components/Footer";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <Layout>
@@ -23,11 +33,13 @@ function App() {
         <SectionFour />
         <SectionFive />
         <SectionSix />
-
+        <SectionSeven />
+        <SectionEight />
+        <SectionNine />
         <Footer />
       </Layout>
     </div>
   );
-}
+};
 
 export default App;
