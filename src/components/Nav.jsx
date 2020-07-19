ed7ef885f846eb3c0a70da8362512dcdd04c760c
dongmin <ehdals0227@gmail.com>
2020-07-20 00:01:56 +0900

    Add: nav 서브 메뉴 추가 적용

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
+import classNames from "classnames";
 import { main, white, black } from "../util/color";
 import styled from "styled-components";
+import { navigationMenuList } from "../util/navMenu";
 
 const Navigation = styled.nav`
   position: fixed;
@@ -28,6 +30,7 @@ const Menu = styled.ul`
     display: flex;
     align-items: center;
   }
+  li.show-border,
   li:hover {
     border-bottom: 2px solid ${black};
   }
@@ -36,7 +39,9 @@ const Menu = styled.ul`
   }
   li a {
     display: inline-block;
-    min-width: 80px;
+    min-width: 100px;
+    font-size: 15px;
+    font-weight: 700;
     padding: 0 10px;
     height: 100%;
     line-height: 80px;
@@ -61,30 +66,106 @@ const Menu = styled.ul`
   }
 `;
 
+const SubNavigation = styled.div`
+  position: absolute;
+  display: flex;
+  width: 100%;
+  left: 0;
+  top: 80px;
+  padding: 60px 0 40px;
+  background-color: ${white};
+  .sub-nav-wrap {
+    display: flex;
+    width: 1080px;
+    margin: 0 auto;
+    justify-content: space-between;
+    text-align: center;
+  }
+  .sub-title {
+    width: 100%;
+  }
+  .sub-title > a {
+    display: inline-block;
+    font-size: 17px;
+    font-weight: 700;
+    text-decoration: none;
+    margin-bottom: 20px;
+  }
+  .sub-title > a:hover,
+  .sub-list > li > a:hover {
+    text-decoration: underline;
+  }
+  .sub-list > li {
+    margin-bottom: 24px;
+  }
+  .sub-list > li:last-child {
+    margin-bottom: 0;
+  }
+  .sub-list > li > a {
+    font-size: 13px;
+    text-decoration: none;
+  }
+`;
+
 const Nav = () => {
+  const [hoverMenu, setHoverMnue] = useState(null);
   return (
     <Navigation>
-      <div className="nav-wrap">
-        <img src="/images/logo_black.svg" alt="카카오뱅크 로고" />
+      <div className="nav-wrap" onMouseLeave={() => setHoverMnue(null)}>
+        <a href="/">
+          <img src="/images/logo_black.svg" alt="카카오뱅크 로고" />
+        </a>
         <Menu>
-          <li>
+          <li
+            className={classNames({ "show-border": hoverMenu === "corp" })}
+            onMouseEnter={() => setHoverMnue("corp")}
+          >
             <a href="https://kakaobank.com/">은행소개</a>
           </li>
-          <li>
+          <li
+            className={classNames({ "show-border": hoverMenu === "products" })}
+            onMouseEnter={() => setHoverMnue("products")}
+          >
             <a href="https://kakaobank.com/">상품안내</a>
           </li>
-          <li>
+          <li
+            className={classNames({ "show-border": hoverMenu === "help" })}
+            onMouseEnter={() => setHoverMnue("help")}
+          >
             <a href="https://kakaobank.com/">고객센터</a>
           </li>
-          <li>
+          <li onMouseEnter={() => setHoverMnue(null)}>
             <a href="https://kakaobank.com/">이벤트</a>
           </li>
-          <li className="send-document">
+          <li className="send-document" onMouseEnter={() => setHoverMnue(null)}>
             <a href="https://kakaobank.com/">
               <span>서류제출하기</span>
             </a>
           </li>
         </Menu>
+        {hoverMenu && (
+          <SubNavigation>
+            <ul className="sub-nav-wrap">
+              {navigationMenuList?.[hoverMenu].map((menu, i) => {
+                return (
+                  <li key={i} className="sub-title">
+                    <a href={menu.detailMenu?.[0]?.link}>
+                      {menu.title}&nbsp;
+                      {menu.detailMenu === null && ">"}
+                    </a>
+                    <ul className="sub-list">
+                      {menu.detailMenu?.map((list, j) => (
+                        <li key={j}>
+                          <a href={list.link}>{list.name}</a>
+                        </li>
+                      ))}
+                    </ul>
+                  </li>
+                );
+              })}
+            </ul>
+          </SubNavigation>
+        )}
       </div>
     </Navigation>
   );
