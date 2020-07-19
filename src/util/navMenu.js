fc7b6f5244453b0f6ead6598dad2cd751e733635
dongmin <ehdals0227@gmail.com>
2020-07-20 00:01:34 +0900

    Add: nav 메뉴 데이터 분리

---
+++ src/util/navMenu.js
@@ -0,0 +1,127 @@
+export const navigationMenuList = {
+  corp: [
+    {
+      title: "카카오뱅크",
+      detailMenu: [
+        { name: "브랜드", link: "" },
+        { name: "주주사 소개", link: "" },
+        { name: "오시는 길", link: "" },
+        { name: "제휴문의", link: "" },
+      ],
+    },
+    {
+      title: "윤리경영",
+      detailMenu: [
+        { name: "윤리강령", link: "" },
+        { name: "임직원행동기준", link: "" },
+      ],
+    },
+    {
+      title: "투자정보",
+      detailMenu: [
+        { name: "공시정보", link: "" },
+        { name: "재무정보", link: "" },
+      ],
+    },
+    {
+      title: "인재채용",
+      detailMenu: null,
+    },
+    {
+      title: "새소식",
+      detailMenu: [
+        { name: "공지사항", link: "" },
+        { name: "보도자료", link: "" },
+      ],
+    },
+  ],
+  products: [
+    {
+      title: "예적금",
+      detailMenu: [
+        { name: "입출금통장", link: "" },
+        { name: "모임통장", link: "" },
+        { name: "세이프박스", link: "" },
+        { name: "저금통", link: "" },
+        { name: "정기예금", link: "" },
+        { name: "자유적금", link: "" },
+        { name: "입출금통장", link: "" },
+      ],
+    },
+    {
+      title: "대출",
+      detailMenu: [
+        { name: "비상금대출", link: "" },
+        { name: "마이너스 통장대출", link: "" },
+        { name: "신용대출", link: "" },
+        { name: "개인사업자 대출", link: "" },
+        { name: "전월세보증금 대출", link: "" },
+      ],
+    },
+    {
+      title: "서비스",
+      detailMenu: [
+        { name: "내 신용정보", link: "" },
+        { name: "해외송금 보내기", link: "" },
+        { name: "해외송금 받기", link: "" },
+        { name: "프렌즈 체크카드", link: "" },
+      ],
+    },
+    {
+      title: "제휴서비스",
+      detailMenu: [
+        { name: "증권사 주식계좌", link: "" },
+        { name: "제휴 신용카드", link: "" },
+      ],
+    },
+  ],
+  help: [
+    {
+      title: "이용안내",
+      detailMenu: [
+        { name: "자주 묻는 질문", link: "" },
+        { name: "이용시간 안내", link: "" },
+        { name: "ATM 이용안내", link: "" },
+        { name: "금리안내", link: "" },
+        { name: "수수료안내", link: "" },
+        { name: "상담안내", link: "" },
+      ],
+    },
+    {
+      title: "상담하기",
+      detailMenu: [
+        { name: "고객의 소리", link: "" },
+        { name: "1:1 문의", link: "" },
+      ],
+    },
+    {
+      title: "소비자보호",
+      detailMenu: [
+        { name: "소비자보호체계", link: "" },
+        { name: "소비자보호 우수사례", link: "" },
+        { name: "전자민원", link: "" },
+        { name: "민원현황공시", link: "" },
+        { name: "민원사무편람", link: "" },
+        { name: "금융정보", link: "" },
+        { name: "금융사기 관련공시", link: "" },
+        { name: "보안취약점 신고 안내", link: "" },
+      ],
+    },
+    {
+      title: "증명서",
+      detailMenu: [
+        { name: "나의 증명서 발급내역", link: "" },
+        { name: "증명서 진위 확인", link: "" },
+      ],
+    },
+    {
+      title: "약관 ・ 서식",
+      detailMenu: [
+        { name: "약관", link: "" },
+        { name: "서식", link: "" },
+        { name: "상품설명서", link: "" },
+        { name: "상품공시", link: "" },
+      ],
+    },
+  ],
+};
